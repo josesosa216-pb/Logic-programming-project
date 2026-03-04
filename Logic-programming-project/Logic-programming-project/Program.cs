6e4a13b2f4ca54341e70a92ea47c46ce6ca88b21
josesosa216-pb <jose.sosa216@pascualbravo.edu.co>
2026-03-04 13:23:35 -0500

    Primera entrega completada

--- Logic-programming-project/Logic-programming-project/Program.cs
+++ Logic-programming-project/Logic-programming-project/Program.cs
@@ -1,7 +1,78 @@
-﻿internal class Program
+﻿
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        /*
+IPO - Sistema de Clasificación de Pedidos
+
+ENTRADAS:
+- monto: decimal
+- ciudad: string
+- tipoCliente: string
+- cantItems: int
+
+PROCESO:
+1. Si monto >= 150000 Y tipoCliente == "recurrente" → Envío Gratis.
+2. Si cantItems >= 5 O monto >= 300000 → Envío Express.
+3. En otros casos → Envío Estándar.
+4. Si ciudad == "exterior" → agregar costo adicional.
+
+SALIDAS:
+- categoria: string
+- costoEnvio: decimal
+- mensaje informativo al usuario.
+*/
+       
+        decimal monto;
+        string ciudad;
+        string tipoCliente;
+        int cantItems;
+
+        decimal costoEnvio = 0;
+        string categoria = "";
+
+        Console.WriteLine("Ingrese el monto del pedido:");
+        monto = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("¿El envío es nacional o exterior?");
+        string tipoEnvio = Console.ReadLine().ToLower();
+
+        Console.WriteLine("Ingrese el tipo de cliente (nuevo/recurrente):");
+        tipoCliente = Console.ReadLine().ToLower();
+
+        Console.WriteLine("Ingrese la cantidad de items:");
+        cantItems = Convert.ToInt32(Console.ReadLine());
+
+        if (monto >= 150000 && tipoCliente == "recurrente")
+        {
+            categoria = "Envío Gratis";
+            costoEnvio = 0;
+        }
+        else if (cantItems >= 5 || monto >= 300000)
+        {
+            categoria = "Envío Express";
+            costoEnvio = 20000;
+        }
+        else
+        {
+            categoria = "Envío Estándar";
+            costoEnvio = 10000;
+        }
+
+        if (tipoEnvio == "exterior")
+        {
+            costoEnvio += 15000;
+        }
+
+
+        Console.WriteLine("\n--- RESUMEN DEL PEDIDO ---");
+        Console.WriteLine("Monto del pedido: $" + monto);
+        Console.WriteLine("Tipo de cliente: " + tipoCliente);
+        Console.WriteLine("Ciudad destino: " + tipoEnvio);
+        Console.WriteLine("Cantidad de items: " + cantItems);
+        Console.WriteLine("Categoría de envío: " + categoria);
+        Console.WriteLine("Costo de envío: $" + costoEnvio);
     }
-}
+    }
+
--- Logic-programming-project/Logic-programming-project/obj/Release/net10.0/Logic-programming-project.AssemblyInfo.cs
+++ Logic-programming-project/Logic-programming-project/obj/Release/net10.0/Logic-programming-project.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logic-programming-project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ee432978f99bf95de14c2daf28766c1e1a51cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logic-programming-project")]
